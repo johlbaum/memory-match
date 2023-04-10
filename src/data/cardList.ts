d02d3cd48fbb27b85ecd4957268b9728f94059de
johlbaum <julien.ohlbaum@gmail.com>
2023-04-10 11:21:52 +0200

    Creation of data

---
+++ src/data/cardList.ts
@@ -0,0 +1,24 @@
+const cardList = [
+  {
+    id: 1,
+    title: "ace",
+    imgUrl: "",
+  },
+  {
+    id: 2,
+    title: "king",
+    imgUrl: "",
+  },
+  {
+    id: 3,
+    title: "queen",
+    imgUrl: "",
+  },
+  {
+    id: 4,
+    title: "jack",
+    imgUrl: "",
+  },
+];
+
+export default cardList;
