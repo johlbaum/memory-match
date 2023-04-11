011323315d8f9cf08d2c83b342150544913fd0fc
johlbaum <julien.ohlbaum@gmail.com>
2023-04-11 13:05:23 +0200

    add img back to data

--- src/data/cardList.ts
+++ src/data/cardList.ts
@@ -2,26 +2,31 @@ import ace from "../assets/ace.png";
 import king from "../assets/king.png";
 import queen from "../assets/queen.png";
 import jack from "../assets/jack.png";
+import backImg from "../assets/back.png";
 
 export const cardList = [
   {
     id: 1,
     title: "ace",
-    img: ace,
+    imgFront: ace,
+    imgBack: backImg,
   },
   {
     id: 2,
     title: "king",
-    img: king,
+    imgFront: king,
+    imgBack: backImg,
   },
   {
     id: 3,
     title: "queen",
-    img: queen,
+    imgFront: queen,
+    imgBack: backImg,
   },
   {
     id: 4,
     title: "jack",
-    img: jack,
+    imgFront: jack,
+    imgBack: backImg,
   },
 ];
